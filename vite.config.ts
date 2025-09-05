6794fe1c1ff9db72f3981bb6abe97bbf53d65ec7
TheShinySnivy <magicwand452@gmail.com>
2025-09-05 15:12:00 -0700

    test fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ export default defineConfig(({ command }) => {
   // command = 'serve' for dev, 'build' for production
   return {
     plugins: [react()],
-    base: command === "serve" ? "/" : "/react-portfolio/", // dev uses '/', production uses GitHub Pages base
+    base: "/react-portfolio/", // dev uses '/', production uses GitHub Pages base
   };
 });
